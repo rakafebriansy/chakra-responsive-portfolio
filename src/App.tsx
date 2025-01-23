ff688d225170621dbdde7105833c8f199071ded9
rakafebriansy <kerenr445@gmail.com>
2025-01-23 13:42:12 +0700

    feat: top bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
+import { FaGithub, FaInstagram, FaLinkedin, FaMoon, FaSun } from 'react-icons/fa';
 import './App.css'
-import { Button } from "@/components/ui/button"
-import { HStack } from "@chakra-ui/react"
+import { Box, Flex, Heading, IconButton, Spacer, VStack } from "@chakra-ui/react"
+import { ColorModeButton, useColorModeValue } from './components/ui/color-mode';
 
 function App() {
+
   return (
-    <HStack>
-      <Button>Click me</Button>
-      <Button>Click me</Button>
-    </HStack>
+    <Box width='100vw' height='100vh' bg={useColorModeValue("white", "black")} >
+      <VStack p='5' >
+        <Flex w='100%'>
+          <Heading ml='8' size='md' fontWeight='semibold' color='cyan.400'>rakafebriansy</Heading>
+          <Spacer></Spacer>
+          <Flex gap='5' alignItems='center'>
+            <IconButton rounded='full' bg={useColorModeValue('teal.100', 'teal.900')} color={useColorModeValue('teal.900', 'teal.100')}>
+              <FaInstagram />
+            </IconButton>
+            <IconButton rounded='full' bg={useColorModeValue('teal.100', 'teal.900')} color={useColorModeValue('teal.900', 'teal.100')}>
+              <FaGithub />
+            </IconButton>
+            <IconButton rounded='full' bg={useColorModeValue('teal.100', 'teal.900')} color={useColorModeValue('teal.900', 'teal.100')}>
+              <FaLinkedin />
+            </IconButton>
+            <ColorModeButton rounded='full' bg={useColorModeValue('teal.100', 'teal.900')} />
+          </Flex>
+        </Flex>
+      </VStack>
+    </Box>
   );
 }
 
