495478740153bfffe2e5048c7ffac47d328ac2fd
rakafebriansy <kerenr445@gmail.com>
2025-01-23 15:30:03 +0700

    feat: social section

--- src/components/sections/Header.tsx
+++ src/components/sections/Header.tsx
@@ -7,7 +7,7 @@ const Header: React.FC = ({ }) => {
     return (
         <Stack>
             <Circle position='absolute' bg='blue.100' opacity='0.5' w='18rem' h='18rem' alignSelf='flex-end' zIndex='0'></Circle>
-            <Flex zIndex='1' direction={{ base: 'column', lg: 'row' }} p={{ base: '0', lg: '32' }} alignSelf='flex-start'>
+            <Flex gap='8' zIndex='1' direction={{ base: 'column', lg: 'row' }} p={{ base: '0', lg: '32' }} alignSelf='flex-start'>
                 <Box mt={{ base: '16', lg: '0' }} alignItems='flex-start'>
                     <Text fontSize='5xl' fontWeight='semibold'>Hi, I'am</Text>
                     <Text fontSize='7xl' fontWeight='bold' bgGradient='to-r' gradientFrom='cyan.400' gradientVia='blue.500' gradientTo='purple.600' bgClip='text'>Raka Febrian</Text>
--- src/components/sections/Social.tsx
+++ src/components/sections/Social.tsx
@@ -1,8 +1,23 @@
+import { HStack, Icon } from "@chakra-ui/react";
 import React from "react";
+import { FaFacebook, FaGoogle, FaShopify, FaSpotify } from "react-icons/fa";
 
-const Social: React.FC = ({  }) => {
+const Social: React.FC = ({ }) => {
     return (
-        <div className=""></div>
+        <HStack spaceX='24'>
+            <Icon boxSize='3rem'>
+                <FaFacebook />
+            </Icon>
+            <Icon boxSize='3rem'>
+                <FaGoogle />
+            </Icon>
+            <Icon boxSize='3rem'>
+                <FaSpotify />
+            </Icon>
+            <Icon boxSize='3rem'>
+                <FaShopify />
+            </Icon>
+        </HStack>
     );
 }
 export default Social;  
